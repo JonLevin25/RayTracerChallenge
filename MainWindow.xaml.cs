fd611bcf795204e65a7e68b6de2275639c10a94d
Jonathan Levin <jon.levin25@gmail.com>
2023-02-08 02:27:12 +0200

    WPF - pixel drawing working

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RayTracerChallenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WriteableBitmap _writeableBitmap;
+        private Image _image;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _image = new Image();
+            RenderOptions.SetBitmapScalingMode(_image, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(_image, EdgeMode.Aliased);
+            
+            Content = _image;
+            Show();
+
+            var w = (int)ActualWidth;
+            var h = (int)ActualHeight;
+
+            _writeableBitmap = new WriteableBitmap(
+                w,
+                (int)h,
+                96,
+                96,
+                PixelFormats.Bgr32,
+                null);
+
+            _image.Source = _writeableBitmap;
+
+            _image.Stretch = Stretch.None;
+            _image.HorizontalAlignment = HorizontalAlignment.Left;
+            _image.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        // The DrawPixel method updates the WriteableBitmap by using
+        // unsafe code to write a pixel into the back buffer.
+        public void DrawPixel(int x, int y, int r, int g, int b)
+        {
+            try
+            {
+                // Reserve the back buffer for updates.
+                _writeableBitmap.Lock();
+                unsafe
+                {
+                    // Get a pointer to the back buffer.
+                    IntPtr pBackBuffer = _writeableBitmap.BackBuffer;
+
+                    // Find the address of the pixel to draw.
+                    pBackBuffer += y * _writeableBitmap.BackBufferStride;
+                    pBackBuffer += x * 4;
+
+                    // Compute the pixel's color.
+                    int color_data = r << 16; // R
+                    color_data |= g << 8; // G
+                    color_data |= b << 0; // B
+
+                    // Assign the color data to the pixel.
+                    *((int*)pBackBuffer) = color_data;
+                }
+
+                // Specify the area of the bitmap that changed.
+                _writeableBitmap.AddDirtyRect(new Int32Rect(x, y, 1, 1));
+            }
+            finally
+            {
+                // Release the back buffer and make it available for display.
+                _writeableBitmap.Unlock();
+            }
+        }
+    }
+}
